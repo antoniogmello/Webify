9afb383a2ae367553dccbc8385a00627b8b32df2
António Mello <antonio.mello@ulisboa.tecnico.pt>
2025-02-20 11:36:35 +0000

    Last try

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { fileURLToPath, URL} from "url";
-import svgLoader from 'vite-svg-loader';
+import { fileURLToPath, URL } from "url";
+import svgLoader from "vite-svg-loader";
 
 export default defineNuxtConfig({
-  ssr: false,
-
+  nitro: {
+    preset: "static",
+    output: {
+      dir: "dist", 
+    },
+  },
   alias: {
-    'utils': fileURLToPath(new URL('./utils', import.meta.url)),
+    utils: fileURLToPath(new URL("./utils", import.meta.url)),
   },
 
-  compatibilityDate: '2025-02-20',
+  compatibilityDate: "2025-02-20",
   vite: {
     plugins: [svgLoader()],
   },
